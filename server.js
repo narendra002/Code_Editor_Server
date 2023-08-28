366b4d63da9d10bde5426fb4a49c89e33f751efd
narendra002 <81070103+narendra002@users.noreply.github.com>
2023-08-28 19:22:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "*", // Replace with your trusted domain
+    origin: "https://code-editor-delta.vercel.app/", // Replace with your trusted domain
   },
 });
 
