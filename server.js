f8b6c00c0abb6fd95df45820c4c2e25d31f0d96e
narendra002 <81070103+narendra002@users.noreply.github.com>
2023-08-29 11:44:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,6 @@ const io = require("socket.io")(httpServer, {
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"], // Define only your custom headers here
     credentials: true,
-  },
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin": req.headers.origin, // or the specific origin you want to give access to
-      "Access-Control-Allow-Credentials": true
-    };
-    res.writeHead(200, headers);
-    res.end();
   }
 });
 
