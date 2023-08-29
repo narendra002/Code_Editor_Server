b44878e7d1cd1cf70486ae038659005530f22dc6
narendra002 <81070103+narendra002@users.noreply.github.com>
2023-08-29 13:19:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-const app = require("express")();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors()); // Enable CORS for your Express app
+
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
     origin: ["https://code-editor-delta.vercel.app/", "http://localhost:5173"],
     methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"], // Define only your custom headers here
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }
 });
 
+const roomToUsersMap = {}; // Store connected users and rooms
 
-
-// var io = require('socket.io')(httpServer,  { cors: { origin: '*' } });
-// Store connected users and rooms
-const roomToUsersMap = {};
-
-// Function to broadcast user updates
 function broadcastConnectedUsers(roomId) {
   io.to(roomId).emit("connectedUsersUpdated", roomToUsersMap[roomId]);
 }
 
-// Function to broadcast user join
 function broadcastUserJoin(roomId, username) {
   io.to(roomId).emit('userJoined', `${username} joined the room`);
 }
 
-// Function to broadcast user leave
 function broadcastUserLeave(roomId, username) {
   io.to(roomId).emit('userLeave', `${username} left the room`);
 }
+
 let isSocketConnected = false; // Flag to track if the message has been logged
 
 io.on("connection", (socket) => {
@@ -36,6 +35,7 @@ io.on("connection", (socket) => {
     console.log("Socket is Connected");
     isSocketConnected = true;
   }
+
   socket.on('joinRoom', (roomId, username, callback) => {
     const response = {
       success: true,
@@ -43,11 +43,11 @@ io.on("connection", (socket) => {
     callback(response);
 
     socket.join(roomId);
-    
+
     if (!roomToUsersMap[roomId]) {
       roomToUsersMap[roomId] = [];
     }
-    
+
     const user = { roomId, username, id: socket.id };
     roomToUsersMap[roomId].push(user);
 
@@ -80,10 +80,8 @@ io.on("connection", (socket) => {
       }
     }
   });
-  
 
   socket.on("updateCode", (roomId, value) => {
-    // Broadcast the updated code to all clients in the room, excluding the sender
     socket.broadcast.to(roomId).emit("codeUpdated", value);
   });
 });
